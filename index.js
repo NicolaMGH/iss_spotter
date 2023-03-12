4c8e129ff5edbe9ca31ef7e3ef2b712b7b9eb36b
NicolaMGH <va3nky@hotmail.com>
2023-03-11 22:36:15 -0500

    implemented fetchflyovertimes with api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -20,3 +20,14 @@ const { fetchCoordsByIP } = require('./iss');
 
 //   console.log('It worked! Returned coordinates:' , coordinates);
 // });
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
